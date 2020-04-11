ee4f32c834213852c39aa7f6c607f57d97c669bc
Lucquiaud Pierre <lucquiaudpierre@gmail.com>
2020-04-11 23:13:28 +0200

    Récupération de master

---
+++ build/generated-sources/ap-source-output/dao/entity/ClientEntity_.java
@@ -0,0 +1,11 @@
+package dao.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-16T16:10:51")
+@StaticMetamodel(ClientEntity.class)
+public class ClientEntity_ extends UserEntity_ {
+
+
+}
