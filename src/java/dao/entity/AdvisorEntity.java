2c821b8bf11bc702647e504f890a0d27f3e04c49
Lucquiaud Pierre <lucquiaudpierre@gmail.com>
2020-04-25 21:39:42 +0200

    Ajout du conseiller préférentiel

--- src/java/dao/entity/AdvisorEntity.java
+++ src/java/dao/entity/AdvisorEntity.java
@@ -8,8 +8,10 @@ package dao.entity;
 import java.io.Serializable;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -21,8 +23,34 @@ import javax.persistence.Table;
 @DiscriminatorValue("AdvisorEntity")
 public class AdvisorEntity extends UserEntity implements Serializable {
     
+    /**
+     * Supervised clients list 
+     */
+    @OneToMany(mappedBy="advisor")
+    private List<ClientEntity> clients;
+    
+    //////////////////////////
+    // Methods             //
+    ////////////////////////
+    
+    /**
+     * Empty constructor, set all the fields with the default value
+     */
     public AdvisorEntity(){super();}
     
+    /**
+     * Constructor
+     * @param address
+     * @param birthday
+     * @param civility
+     * @param first_name
+     * @param last_name
+     * @param login
+     * @param mail
+     * @param password
+     * @param phone
+     * @throws NoSuchAlgorithmException 
+     */
     public AdvisorEntity(
             String address,
             Date birthday,
@@ -45,5 +73,33 @@ public class AdvisorEntity extends UserEntity implements Serializable {
             password,
             phone
         );
+    }
+
+    /**
+     * Getter on the clients list
+     * @return the supervised clients list
+     */
+    public List<ClientEntity> getClients() {
+        return clients;
+    }
+
+    /**
+     * Add a client to the supervised clients list
+     * @param new_client 
+     */
+    public void addClient(ClientEntity new_client) {
+        if(!this.clients.contains(new_client)){
+            this.clients.add(new_client);
+        }
+    }
+
+    /**
+     * Remove a client from the supervised clients list
+     * @param client 
+     */
+    public void removeClient(ClientEntity client) {
+        if(!this.clients.contains(client)){
+            this.clients.remove(client);
+        }
     }
 }
