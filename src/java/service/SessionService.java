b26befef6142624669d7564bef2418cb469d2044
Lucquiaud Pierre <lucquiaudpierre@gmail.com>
2020-05-01 14:58:37 +0200

    Ajout de la méthode permettant de détruire la session au SessionService, utilisation du SessionService dans le UserController

--- src/java/service/SessionService.java
+++ src/java/service/SessionService.java
@@ -19,7 +19,7 @@ public class SessionService {
      * @param u
      * @return 
      */
-    private HttpSession createSession(HttpServletRequest request, UserEntity u){
+    public HttpSession createSession(HttpServletRequest request, UserEntity u){
         // Crée une nouvelle session si aucune n'existe
         HttpSession session = request.getSession(true);
         
@@ -56,6 +56,19 @@ public class SessionService {
         }
         
         return obj;
+    }
+    
+    /**
+     * Destroy session if it's existing
+     * 
+     * @param request 
+     */
+    public void destroySession(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        
+        if(null != session){
+            session.invalidate();
+        }
     }
     
 }
